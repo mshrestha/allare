5ad31dae49b94802d0849478c53947680f907e5c
Pradeep Bajracharya <pradeep@kazistudios.com>
2018-03-28 12:04:33 +0545

    Added IMCI wasting and updated data.php config

---
+++ app/Models/Data/ImciWasting.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ImciWasting extends Model
+{
+    protected $table = "imci_wasting";
+}
--- config/data.php
+++ config/data.php
@@ -1,46 +1,87 @@
 <?php 
 
 return [
-	'maternal_counselling' => [
-		[
-			'model'		=> 'CcMrCounsellingAnc',
-			'table'		=> 'cc_mr_counselling_anc',
-			'server'	=> 'community',
-			'api_id'	=> 'WfrGlt9gYxW.YF2ivOyo5'
+	'maternal' => [
+		'maternal_counselling' => [
+			[
+				'model'		=> 'CcMrCounsellingAnc',
+				'table'		=> 'cc_mr_counselling_anc',
+				'server'	=> 'community',
+				'api_id'	=> 'WfrGlt9gYxW.YF2ivOyo5'
+			],
+			[
+				'model'		=> 'CcMrAncNutriCounsel',
+				'table'		=> 'cc_mr_anc_nutri_counsel',
+				'server'	=> 'community',
+				'api_id'	=> 'c0zIiLT97P4',
+			],
 		],
-		[
-			'model'		=> 'CcMrAncNutriCounsel',
-			'table'		=> 'cc_mr_anc_nutri_counsel',
-			'server'	=> 'community',
-			'api_id'	=> 'c0zIiLT97P4',
+		'plw_who_receive_ifas' => [
+			[
+				'model'		=> 'CcMrAncIfaDistribution',
+				'table'		=> 'cc_mr_anc_ifa_distribution',
+				'server'	=> 'community',
+				'api_id'	=> 'D9jDxIOFAwV'
+			],
+		],
+
+		'pregnant_women_weighed' => [
+			[
+				'model'		=> 'CcMrWeightInKgAnc',
+				'table'		=> 'cc_mr_weight_in_kg_anc',
+				'server'	=> 'community',
+				'api_id'	=> 'WfrGlt9gYxW.OJd05AWCFTk'
+			],
 		]
 	],
-	'plw_who_receive_ifas' => [
-	],
-	'pregnant_women_weighed' => [
-	],
-	'child_growth_monitoring' => [
-	],
-	'iycf_counselling' => [
-		[
-			'model'		=> 'ImciCounselling',
-			'table'		=> 'imci_counselling',
-			'server'	=> 'central',
-			'api_id'	=> 'RN4w5hxGHRk'
+	'child' => [
+		'child_growth_monitoring' => [
 		],
-		[
-			'model'		=> 'ImciFemale',
-			'table'		=> 'imci_female',
-			'server'	=> 'central',
-			'api_id'	=> 'CRZkTTALRSb'
+		'iycf_counselling' => [
+			[
+				'model'		=> 'ImciCounselling',
+				'table'		=> 'imci_counselling',
+				'server'	=> 'central',
+				'api_id'	=> 'RN4w5hxGHRk'
+			],
+			[
+				'model'		=> 'ImciFemale',
+				'table'		=> 'imci_female',
+				'server'	=> 'central',
+				'api_id'	=> 'CRZkTTALRSb'
+			],
+			[
+				'model'		=> 'ImciMale',
+				'table'		=> 'imci_male',
+				'server'	=> 'central',
+				'api_id'	=> 'Fnsa8A43USl'
+			]
 		],
-		[
-			'model'		=> 'ImciMale',
-			'table'		=> 'imci_male',
-			'server'	=> 'central',
-			'api_id'	=> 'Fnsa8A43USl'
+		'vitamin_a_supplementation' => [
+			[
+				'model'		=> 'CcCrAdditionalFoodSupplimentation',
+				'table'		=> 'cc_cr_additional_food_supplimentation',
+				'server'	=> 'community',
+				'api_id'	=> 'gpJQzatKvmH'	
+			],
 		]
 	],
-	'vitamin_a_supplementation' => [
+	
+	'imci_stunting' => [
+		[
+			'model'		=> 'ImciStunting',
+			'table'		=> 'imci_stunting',
+			'server'	=> 'central',
+			'api_id'	=> 'QCGhrXxmJDb'	
+		],
+	],
+	'imci_wasting' => [
+		[
+			'model'		=> 'ImciWasting',
+			'table'		=> 'imci_wasting',
+			'server'	=> 'central',
+			'api_id'	=> 'JBhv2WOYxSf'	
+		],
 	]
+
 ];
