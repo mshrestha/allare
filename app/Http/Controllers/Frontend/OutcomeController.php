b4e8dabf13a735ca281df5bc343152369810c6c0
Pradeep Bajracharya <pradeep@kazistudios.com>
2018-04-01 19:24:43 +0545

    Outcomes fetched according to the details in the select form

--- app/Http/Controllers/Frontend/OutcomeController.php
+++ app/Http/Controllers/Frontend/OutcomeController.php
@@ -45,17 +45,25 @@ class OutcomeController extends Controller
 		$data = config('data.outcomes');
 		$requestData = $request->all();
 		$indicator = $request->indicator_id;
+		$source = $request->department_id;
 		$ou = explode('.', $request->organisation_unit_id);
-		dd($data[$indicator][0][
-			'server']);
-		if(($data[$indicator][0]['server'] == 'central')
+		// dd($data[$indicator][0]['server']);
+		if($data[$indicator][0]['server'] == 'central')
 			$ou = $ou[0];
 		if($data[$indicator][0]['server'] == 'community')
 			$ou = $ou[1];
 		$pe = $this->getPeriodArray($request->period_id);
-		for ($i=0; $i < count($data[$indicator]); $i++) { 
-			
+		// for ($i=0; $i < count($data[$indicator]); $i++) { 
+		// 	dd($data[$indicator][$i]);
+		// }
+
+		$all_maternal_table_datas = [];
+		foreach ($data[$indicator] as $table) {
+			$model = 'App\Models\Data\\' . $table['model'];
+			$data = $model::whereIn('period', $pe)->where('source', $source)->where('organisation_unit', $ou)->whereNull('category_option_combo')->get();
+			array_push($all_maternal_table_datas, $data);
 		}
+		dd($all_maternal_table_datas);
 
 	}
 
