8ff8789a5f6e8990dbc1ee76ff7241bba2c40611
Pradeep Bajracharya <pradeep@kazistudios.com>
2018-04-06 11:29:38 +0545

    Scheduler defined to collect data monthly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->call('App\Http\Controllers\ImportData\ImporterController@importer')->monthly();
+        $schedule->call('App\Http\Controllers\ImportData\ImporterController@scheduleImport')->monthly();
     }
 
     /**
--- app/Http/Controllers/ImportData/ImporterController.php
+++ app/Http/Controllers/ImportData/ImporterController.php
@@ -32,6 +32,7 @@ class ImporterController extends Controller
     use PeriodHelper;
 
     public function import() {
+        // dd($period);
     	$data = config('datamodel');
     	$flag = 0;
     	for ($k=0; $k < count($data); $k++) {
@@ -134,6 +135,113 @@ class ImporterController extends Controller
             $model::insert($save_array);
             echo 'ok';
     	}
+        dd('done');
+    }
+
+
+    public function scheduleImport() {
+        // dd($period);
+        $data = config('datamodel');
+        $flag = 0;
+        for ($k=0; $k < count($data); $k++) {
+            $currData = $data[$k];
+            // dd($currData);
+            $save_array = [];
+            $ou = '';
+            if($currData['server'] == 'central') {
+                $ou = config('static.centralOrganisation');
+            }else if($currData['server'] == 'community') {
+                $ou = config('static.communityOrganisation');
+            }
+
+            $pe = 'LAST_MONTH';
+            for($j = 0; $j < count($ou); $j++) {
+                $baseUrl = config('static.centralBaseUrl');
+                if($currData['server'] == 'central')
+                    $baseUrl = config('static.centralBaseUrl');
+                else if($currData['server'] == 'community')
+                    $baseUrl = config('static.communityBaseUrl');
+                $url = $baseUrl.config('static.analyticsEP').'?dimension=dx:'.$currData['api_id'].'&dimension=pe:LAST_MONTH&filter=ou:'.$ou[$j].'&displayProperty=NAME&outputIdScheme=UID&skipData=True';
+
+                $responses = $this->callUrl($url);
+                $responses = json_decode($responses);
+                
+                // dd($url);
+                // dd($responses);
+                $metaData = $responses->metaData;
+                
+                $co = $metaData->dimensions->co;
+
+                $dx = $currData['api_id'].';';
+                if(count($co) > 0) {
+                    if($co != 'dCWAvZ8hcrs') {
+                        $flag = 1;
+                        for($i = 0; $i < count($co); $i++) {
+                            $dx .= $currData['api_id'].'.'.$co[$i].';';
+                        }
+                    }
+                }
+                // dd($co);
+                $dx = rtrim($dx, ';');
+                $url = $baseUrl.config('static.analyticsEP').'.json?dimension=dx:'.$dx.'&dimension=pe:'.$pe.'&filter=ou:'.$ou[$j].'&displayProperty=NAME&outputIdScheme=UID';
+                $responses = $this->callUrl($url);
+                $responses = json_decode($responses);
+                // dd($url);
+                // dd($responses);
+                $metaData = $responses->metaData;
+                $rows = $responses->rows;
+                foreach ($rows as $keyrows => $row) {
+                    $unit = [];
+                    // $ouId = -1;
+                    // if($ou[$j] == 'op5gbhjVCRk') {
+                    //  $orgUnit = OrganizationUnit::where('id','R1GAfTe6Mkb')->first();
+                    //  $ouId = $orgUnit->id;
+                    // }
+                    $unit['organisation_unit'] = $ou[$j];
+                    foreach ($row as $key => $value) {
+                        if($key == 0) {
+                            
+                            if($flag == 1) {
+                                $co = explode('.',$value);
+                                if(count($co) > 1) {
+                                    $co = $co[1];
+                                    // print_r($co); echo $url.'<br /><br />';
+                                }else{
+                                    $co = NULL;
+                                }
+
+                                // $flag = 0;
+
+                            }else{
+                                $co = NULL;
+                            }
+                            // $unit['name'] = $metaData->items->$value->name;
+                            $unit['category_option_combo'] = $co;
+                            
+                        }
+                        else if($key == 1) {
+                            $unit['period'] = $value?:'';
+                            $unit['period_name'] = $metaData->items->$value->name;
+                        }
+                        else if($key == 2) {
+                            $unit['value'] = $value;
+                        }
+                    }
+                    $unit['source'] = $currData['source'];
+                    $unit['server'] = $currData['server'];
+                    $unit['import_date'] = date('Y-m-d');
+                    $unit['created_at'] = date('Y-m-d H:i:s');
+                    $unit['updated_at'] = date('Y-m-d H:i:s');
+                    array_push($save_array,$unit);
+                    // dd($save_array);
+                }
+                    
+            }
+            
+            $model = 'App\Models\Data\\'.$currData['model'];
+            $model::insert($save_array);
+        }
+        // dd($save_array);
         dd('done');
     }
 }
