ef44fbbe16264717ff5d69dc4d23ffd2c1acf787
Shivani <shivani@kazistudios.com>
2018-04-10 11:29:04 +0545

    Dashboard update

--- resources/views/frontend/dashboard/index.blade.php
+++ resources/views/frontend/dashboard/index.blade.php
@@ -2,6 +2,16 @@
 @section('content')
 	<div class="container">
 		<div class="row">
+			<div class="col-md-9">
+				<h1>MAP</h1>
+			
+			</div>
+			<div class="col-md-3">
+				
+			</div>
+		</div> <!-- row -->
+		
+		<div class="row">
 			<!-- content -->
 			<div class="col-sm-9">
 				<div class="intro-text h3">
