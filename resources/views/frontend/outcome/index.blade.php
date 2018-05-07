e5b563a5f328108daabba48bd2bb0c88b751acf3
Nikesh Ulak <nikesh@kazistudios.com>
2018-05-07 16:49:31 +0545

    outcome swiper added.

--- resources/views/frontend/outcome/index.blade.php
+++ resources/views/frontend/outcome/index.blade.php
@@ -1,6 +1,134 @@
 @extends('layouts.app')
 @section('content')
 	<div class="container">
+
+    {{-- tabcontent start  --}}
+
+    <div class="tab-content mt-3">
+      <div class="row">
+        <div class="col-12">
+          <div class="box-heading float-left ml-0">OUTCOMES</div>
+          <div class="swiper-tab-nav">
+            <ul class="list-inline">
+              <li class="list-inline-item">
+                <a href="#slide0">STUNTING</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#slide1">WASTING</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#slide2">EXCLUSIVE BREASTFEEDING</a>
+              </li>
+            </ul>
+          </div> {{-- swiper-tab-nav --}}
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          {{-- tab slide swiper --}}
+          <!-- Swiper -->
+          <div class="swiper-container swiper-tab" id="swiper-tab-outcome">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide" data-hash="slide0">
+                <div class="row">
+                  <div class="col-xl-2 col-lg-3 col-sm-4">
+                    <div class="tab-col-title">title</div>
+                    <div class="tab-col-subtitle"> date</div>
+                    <div id="canvas-holder">
+                      CHART GOES HERE
+                    </div>
+                  </div> {{-- col-xs-2 --}}
+                  <div class="col-xl-5 offset-xl-1 pr-xl-0 col-lg-6 col-sm-8">
+                    <div class="tab-col-title">People Counselled on Maternal Health</div>
+                    <div class="tab-col-subtitle">Jan 14 - Feb 18</div>
+                    <div id="canvas-holder">
+                      LINECHART HERE
+                    </div>
+                  </div> {{-- col-xs-5 --}}
+                  <div class="col-xl-3 offset-xl-1 report-col col-lg-3">
+                    <div class="tab-col-title">Reports on Maternal Health</div>
+                    <ul class="report-list row">
+                      <li class="col-sm-6 col-lg-12">
+                        <a href="#" class="row">
+                          <span class="col-md-3 col-sm-3 col-3 pr-0"><img src="{{asset('images\report-img.png')}}" alt="" class="img-fluid"> </span>
+                          <span class="col-sm-9 col-md-9 col-9"><span class="report-title">Lorem ipsum dolor sit amet adipiscing</span> <span class="date">Feb 13,2018</span></span>
+                        </a>
+                      </li>
+                      <li class="col-sm-6 col-lg-12">
+                        <a href="#" class="row">
+                          <span class="col-md-3 col-sm-3 col-3 pr-0"><img src="{{asset('images\report-img.png')}}" alt="" class="img-fluid"> </span>
+                          <span class="col-sm-9 col-md-9 col-9"><span class="report-title">Lorem ipsum dolor sit amet adipiscing</span> <span class="date">Feb 13,2018</span></span>
+                        </a>
+                      </li>
+                      <li class="col-sm-6 col-lg-12">
+                        <a href="#" class="row">
+                          <span class="col-md-3 col-sm-3 col-3 pr-0"><img src="{{asset('images\report-img.png')}}" alt="" class="img-fluid"> </span>
+                          <span class="col-sm-9 col-md-9 col-9"><span class="report-title">Lorem ipsum dolor sit amet adipiscing</span> <span class="date">Feb 13,2018</span></span>
+                        </a>
+                      </li>
+                      
+                    </ul>
+
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-slide" data-hash="slide1">
+                <div class="row">
+                  <div class="col-xl-2 col-lg-3 col-sm-4">
+                    <div class="tab-col-title">title</div>
+                    <div class="tab-col-subtitle"> date</div>
+                    <div id="canvas-holder">
+                      CHART GOES HERE
+                    </div>
+                  </div> {{-- col-xs-2 --}}
+                  <div class="col-xl-5 offset-xl-1 pr-xl-0 col-lg-6 col-sm-8">
+                    <div class="tab-col-title">People Counselled on Maternal Health</div>
+                    <div class="tab-col-subtitle">Jan 14 - Feb 18</div>
+                    <div id="canvas-holder">
+                      LINECHART HERE
+                    </div>
+                  </div> {{-- col-xs-5 --}}
+                  <div class="col-xl-3 offset-xl-1 report-col col-lg-3">
+                    <div class="tab-col-title">Reports on Maternal Health</div>
+                    <ul class="report-list row">
+                      <li class="col-sm-6 col-lg-12">
+                        <a href="#" class="row">
+                          <span class="col-md-3 col-sm-3 col-3 pr-0"><img src="{{asset('images\report-img.png')}}" alt="" class="img-fluid"> </span>
+                          <span class="col-sm-9 col-md-9 col-9"><span class="report-title">Lorem ipsum dolor sit amet adipiscing</span> <span class="date">Feb 13,2018</span></span>
+                        </a>
+                      </li>
+                      <li class="col-sm-6 col-lg-12">
+                        <a href="#" class="row">
+                          <span class="col-md-3 col-sm-3 col-3 pr-0"><img src="{{asset('images\report-img.png')}}" alt="" class="img-fluid"> </span>
+                          <span class="col-sm-9 col-md-9 col-9"><span class="report-title">Lorem ipsum dolor sit amet adipiscing</span> <span class="date">Feb 13,2018</span></span>
+                        </a>
+                      </li>
+                      <li class="col-sm-6 col-lg-12">
+                        <a href="#" class="row">
+                          <span class="col-md-3 col-sm-3 col-3 pr-0"><img src="{{asset('images\report-img.png')}}" alt="" class="img-fluid"> </span>
+                          <span class="col-sm-9 col-md-9 col-9"><span class="report-title">Lorem ipsum dolor sit amet adipiscing</span> <span class="date">Feb 13,2018</span></span>
+                        </a>
+                      </li>
+                      
+                    </ul>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination invisible"></div>  
+            <!-- Add Arrows -->
+            <div class="swiper-button-next invisible"></div>
+            <div class="swiper-button-prev invisible"></div>
+          </div>
+          {{-- tab slide swiper end --}}
+        </div>
+      </div>
+  </div>
+
+  {{-- tabcontent end --}}
+
 		{{-- @include('layouts.partials.main-chart-partial') --}}
        {{-- {{dd($trend_analysis)}} --}}
 		@foreach($trend_analysis as $key => $analysis)
@@ -201,5 +329,22 @@
         }
       });
     }
+  </script>
+  <script src="{{asset('js/swiper.min.js')}}"></script>
+  <script>
+     var swiper = new Swiper('#swiper-tab-outcome', {
+      spaceBetween: 30,
+      hashNavigation: {
+        watchState: true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
   </script>
 @endsection
--- resources/views/layouts/partials/trend-analysis-chart-partial.blade.php
+++ resources/views/layouts/partials/trend-analysis-chart-partial.blade.php
@@ -1,31 +1,43 @@
 <div class="swiper-slide" data-hash="slide{{$key}}">
   <div class="row">
-    <div class="col-xl-2">
+    <div class="col-xl-2 col-lg-3 col-sm-4">
       <div class="tab-col-title">{{$analysis['name']}}</div>
       <div class="tab-col-subtitle"> {{strtoupper($analysis['current_month'])}}</div>
       <div id="canvas-holder">
 				<canvas id="chart-area-{{ $key }}"></canvas>
 			</div>
     </div> {{-- col-xs-2 --}}
-    <div class="col-xl-5">
+    <div class="col-xl-5 offset-xl-1 pr-xl-0 col-lg-6 col-sm-8">
       <div class="tab-col-title">People Counselled on Maternal Health</div>
       <div class="tab-col-subtitle">Jan 14 - Feb 18</div>
       <div id="canvas-holder">
 				<canvas id="line-chart-{{ $key }}"></canvas>
 			</div>
     </div> {{-- col-xs-5 --}}
-    <div class="col-xl-5">
-	    <div class="row">
-		    <div class="col-xl-5">
-		      <div class="tab-col-title">Reports on Maternal Health</div>
-		      <div class="tab-col-subtitle">Jan 14 - Feb 18</div>
-		      
-		    </div> {{-- col-xs-5 --}}
-		    <div class="col-xl-5">
-		      <div class="tab-col-title">Reports on Maternal Health</div>
-		        <div class="report-list">repot list</div>
-		    </div> {{-- col-xs-5 --}}
-	    </div>
+    <div class="col-xl-3 offset-xl-1 report-col col-lg-3">
+			<div class="tab-col-title">Reports on Maternal Health</div>
+		  <ul class="report-list row">
+		  	<li class="col-sm-6 col-lg-12">
+		  		<a href="#" class="row">
+			  		<span class="col-md-3 col-sm-3 col-3 pr-0"><img src="{{asset('images\report-img.png')}}" alt="" class="img-fluid"> </span>
+			  		<span class="col-sm-9 col-md-9 col-9"><span class="report-title">Lorem ipsum dolor sit amet adipiscing</span> <span class="date">Feb 13,2018</span></span>
+		  		</a>
+		  	</li>
+		  	<li class="col-sm-6 col-lg-12">
+		  		<a href="#" class="row">
+			  		<span class="col-md-3 col-sm-3 col-3 pr-0"><img src="{{asset('images\report-img.png')}}" alt="" class="img-fluid"> </span>
+			  		<span class="col-sm-9 col-md-9 col-9"><span class="report-title">Lorem ipsum dolor sit amet adipiscing</span> <span class="date">Feb 13,2018</span></span>
+		  		</a>
+		  	</li>
+		  	<li class="col-sm-6 col-lg-12">
+		  		<a href="#" class="row">
+			  		<span class="col-md-3 col-sm-3 col-3 pr-0"><img src="{{asset('images\report-img.png')}}" alt="" class="img-fluid"> </span>
+			  		<span class="col-sm-9 col-md-9 col-9"><span class="report-title">Lorem ipsum dolor sit amet adipiscing</span> <span class="date">Feb 13,2018</span></span>
+		  		</a>
+		  	</li>
+		  	
+		  </ul>
+
     </div>
   </div>
 </div>
