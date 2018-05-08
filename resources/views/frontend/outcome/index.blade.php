8aab715dba686655e7aeaf25729ca84d06840192
Pradeep Bajracharya <pradeep@kazistudios.com>
2018-05-08 12:24:58 +0545

    Replaced outcome chart with d3 js chart

--- resources/views/frontend/outcome/index.blade.php
+++ resources/views/frontend/outcome/index.blade.php
@@ -213,23 +213,24 @@
         dataCSV.push(temp);
       };
 
-      var data = [
-          { x: 0, y: 10, },
-          { x: 1, y: 15, },
-          { x: 2, y: 35, },
-          { x: 3, y: 20, },
-      ];
+      dataCSV.forEach(function(d) {
+        d.date = d3.time.format("%Y").parse(d.date);
+      });
 
       var margin = {top: 20, right: 20, bottom: 30, left: 50},
-          width = 575 - margin.left - margin.right,
-          height = 350 - margin.top - margin.bottom;
+          width = 500 - margin.left - margin.right,
+          height = 300 - margin.top - margin.bottom;
 
-      var x = d3.scale.linear()
-          .domain([0, d3.max(data, function(d) { return d.x; })])
-          .range([0, width]);
+      var x = d3.time.scale()
+                .range([0, width])
+                .domain([d3.max(dataCSV, function(d) { return d.date; }), d3.min(dataCSV, function(d) { return d.date; })]);
+
+      // var x = d3.scale.linear()
+      //     .domain([0, d3.max(dataCSV, function(d) { return d.date; })])
+      //     .range([0, width]);
 
       var y = d3.scale.linear()
-          .domain([0, d3.max(data, function(d) { return d.y; })])
+          .domain([0, d3.max(dataCSV, function(d) { return d.value; })])
           .range([height, 0]);
 
       var xAxis = d3.svg.axis()
@@ -241,9 +242,9 @@
           .orient("left");
 
       var area = d3.svg.area()
-          .x(function(d) { return x(d.x); })
+          .x(function(d) { return x(d.date); })
           .y0(height)
-          .y1(function(d) { return y(d.y); });
+          .y1(function(d) { return y(d.value); });
 
       var svg = d3.select("#line-chart-"+id)
           .attr("width", width + margin.left + margin.right)
@@ -252,18 +253,18 @@
           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
       svg.append("path")
-          .datum(data)
+          .datum(dataCSV)
           .attr("class", "area")
           .attr("d", area);
 
-      svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
+      // svg.append("g")
+      //     .attr("class", "x axis")
+      //     .attr("transform", "translate(0," + height + ")")
+      //     .call(xAxis);
 
-      svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis);
+      // svg.append("g")
+      //     .attr("class", "y axis")
+      //     .call(yAxis);
 
     }
   </script>
