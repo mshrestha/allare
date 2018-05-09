d75bea32ac413ce2105044ea2f524c4948646abd
Nikesh Ulak <nikesh@kazistudios.com>
2018-05-09 18:20:27 +0545

    area chart height increased.

--- resources/views/frontend/outcome/partials/outcome-trend-analysis-partial.blade.php
+++ resources/views/frontend/outcome/partials/outcome-trend-analysis-partial.blade.php
@@ -33,19 +33,19 @@
 				</div>
       </div>
     </div> {{-- col-xs-2 --}}
-    <div class="col-xl-5 offset-xl-1 pr-xl-0 col-lg-6 col-sm-8">
+    <div class="col-xl-6 offset-xl-1 pr-xl-0 col-lg-6 col-sm-8">
       <div class="tab-col-title">People Counselled on {!! str_replace('_', ' ', $key) !!}</div>
 {{-- <<<<<<< HEAD
       <div class="tab-col-subtitle">Jan 14 - Feb 18</div>
       <div class="canvas-holder">
 ======= --}}
       <div class="tab-col-subtitle area-date">Jan 14 - Feb 18</div>
-      <div class="canvas-holder" id="area-chart-{{$counter}}">
+      <div class="canvas-holder area-chart" id="area-chart-{{$counter}}">
 {{-- >>>>>>> 1d0f6dc195de0557c17d27d1f1676607ccbfcee0 --}}
         <svg id="line-chart-{{$counter}}"></svg>
       </div>
     </div> {{-- col-xs-5 --}}
-    <div class="col-xl-3 offset-xl-1 report-col col-lg-3">
+    <div class="col-xl-3 report-col col-lg-3">
       <div class="tab-col-title">Reports on {!! str_replace('_', ' ', $key) !!}</div>
       <ul class="report-list row">
         <li class="col-sm-6 col-lg-12">
--- resources/views/layouts/partials/trend-analysis-chart-partial.blade.php
+++ resources/views/layouts/partials/trend-analysis-chart-partial.blade.php
@@ -12,19 +12,19 @@
 				<svg id="chart-area-{{ $key }}"></svg>
 			</div>
     </div> {{-- col-xs-2 --}}
-    <div class="col-xl-5 pr-xl-0 col-lg-6 col-sm-8">
+    <div class="col-xl-6 pr-xl-0 col-lg-6 col-sm-8">
       <div class="tab-col-title">People Counselled on {{$analysis['name']}}</div>
 {{-- <<<<<<< HEAD
       <div class="tab-col-subtitle">Jan 14 - Feb 18</div>
       <div class="canvas-holder">
 ======= --}}
       <div class="tab-col-subtitle area-date">Jan 14 - Feb 18</div>
-      <div class="canvas-holder" id="area-chart-{{ $key }}">
+      <div class="canvas-holder area-chart" id="area-chart-{{ $key }}">
 {{-- >>>>>>> 1d0f6dc195de0557c17d27d1f1676607ccbfcee0 --}}
 				<svg id="line-chart-{{ $key }}"></svg>
 			</div>
     </div> {{-- col-xs-5 --}}
-    <div class="col-xl-3 offset-xl-1 report-col col-lg-3">
+    <div class="col-xl-3 report-col col-lg-3">
 			<div class="tab-col-title">Reports on {{$analysis['name']}}</div>
 		  <ul class="report-list row">
 		  	<li class="col-sm-6 col-lg-12">
