453f5fd6ab9734c3c9832c61d7188e206d6c48f3
Ruju Bajracharya <bajra.ruju@gmail.com>
2018-06-04 18:15:49 +0545

    trend analysis line area chart changed to chart js

--- resources/views/frontend/outcome/child.blade.php
+++ resources/views/frontend/outcome/child.blade.php
@@ -11,35 +11,10 @@
 		<div class="row">
 			<div class="col-12">
 				<div class="box-heading float-left ml-0 mr-1">CHILD</div>
-				{{-- <div class="swiper-tab-nav">
-					<ul class="list-inline">
-						<li class="list-inline-item">
-							<a href="#slide0" class="swipernav nav-slide0 active">IFCF COUNSELLING</a>
-						</li>
-						<li class="list-inline-item">
-							<a href="#slide1" class="swipernav nav-slide1">SUPPLEMENTS</a>
-						</li>
-					</ul>
-				</div> --}} {{-- swiper-tab-nav --}}
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-12">
-				{{-- tab slide swiper --}}
-				<!-- Swiper -->
-				{{-- <div class="swiper-container swiper-tab" id="swiper-tab-child-output">
-					<div class="swiper-wrapper">
-						@foreach($trend_analysis as $key => $analysis)
-						@include('layouts.partials.trend-analysis-chart-partial')
-						@endforeach
-					</div>
-					<!-- Add Pagination -->
-					<div class="swiper-pagination invisible"></div>  
-					<!-- Add Arrows -->
-					<div class="swiper-button-next invisible"></div>
-					<div class="swiper-button-prev invisible"></div>
-				</div> --}}
-				{{-- tab slide swiper end --}}
 				<div class="trend-div">
 					@foreach($trend_analysis as $key => $analysis)
 						@include('layouts.partials.trend-analysis-chart-partial')
@@ -203,100 +178,51 @@
 		trendAnalysisChart('{{ $key }}', arr)
 	@endforeach
 
-	var startDate, endDate;
 	function trendAnalysisChart(id, data_value) {
-		// console.log(data_value);
-		var interpolateTypes = ['linear','step-before','step-after','basis','basis-open','basis-closed','bundle','cardinal','cardinal-open','cardinal-closed','monotone'];
-		var randomScalingFactor = function() {
-			return Math.round(Math.random() * 100);
-		};
-		var dataCSV = [];
-		var max = 0;
-		var origValues = [];
-		var processedValues = [];
-		startDate = data_value.periods[0];
-		endDate = data_value.periods[data_value.periods.length - 1];
-		console.log(startDate, endDate);
-		
-		for (var i = data_value.values.length - 1; i > 0; i--) {
-			if(i == 0) {
-				console.log('zero');
-				console.log(data_value.periods[i]);
+		var ctx = document.getElementById('line-area-' + id).getContext('2d');
+		var config = {
+			type: 'line',
+			data: {
+				labels: data_value.periods,
+				datasets: [{
+					label: data_value.name,
+					borderColor: '#81ddc6',
+					backgroundColor: '#81ddc6',
+					data: data_value.values,
+				}]
+			},
+			options: {
+				responsive: true,
+				maintainAspectRatio: false,
+				title: {
+					display: false,
+					text: 'Chart.js Line Chart - Stacked Area'
+				},
+				tooltips: {
+					mode: 'index',
+				},
+				hover: {
+					mode: 'index'
+				},
+				scales: {
+					xAxes: [{
+						scaleLabel: {
+							display: true,
+							// labelString: 'Month'
+						}
+					}],
+					yAxes: [{
+						stacked: true,
+						scaleLabel: {
+							display: true,
+							// labelString: 'Value'
+						}
+					}]
+				}
 			}
-			temp = {};
-			temp.date = data_value.periods[i];
-			temp.value = data_value.values[i];
-			dataCSV.push(temp);
 		};
-		maxDate = 0;
-		minDate = 999999;
-		dataCSV.forEach(function(d) {
-			temp = 0;
-			if(d.value.includes('E')){
-				parts = d.value.split('E');
-				temp = (parseFloat(parts[0]) * Math.pow(10,parseFloat(parts[1])));
-			}
-			else
-				temp = parseInt(d.value);
-			if(max < parseInt(temp))
-				max = d.value;
-			if(maxDate < parseInt(d.date))
-				maxDate = parseInt(d.date);
-			if(minDate > parseInt(d.date))
-				minDate = parseInt(d.date);
-		});
-		var parentDiv = document.getElementById('output-area-chart-'+id);
-		var w = parentDiv.clientWidth,                        
-		h = parentDiv.clientHeight;   
-		var margin = {top: 20, right: 20, bottom: 20, left: 90},
-		width = w - margin.left - margin.right,
-		height = h - margin.top - margin.bottom;
-
-		var x = d3.time.scale()
-		.range([0, width])
-		.domain([minDate, maxDate]);
+		window.myLine = new Chart(ctx, config);
 		
-		var y = d3.scale.linear()
-		.domain([0, max])
-		.range([height, 0]);
-
-		var xAxis = d3.svg.axis()
-		.scale(x)
-		.orient("bottom")
-		.innerTickSize(-height)
-		.outerTickSize(0)
-		.ticks(5)
-		.tickPadding(20);
-
-		var yAxis = d3.svg.axis()
-		.scale(y)
-		.orient("left")
-		.innerTickSize(-width)
-		.outerTickSize(0)
-		.ticks(5)
-		.tickPadding(20);
-
-		var area = d3.svg.area()
-		.x(function(d) { return x(d.date); })
-		.y0(height)
-		.y1(function(d) { return y(d.value); })
-		.interpolate(interpolateTypes[6]);
-
-		var svg = d3.select("#line-chart-"+id)
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.attr("class", "areachart")
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-		svg.append("path")
-		.datum(dataCSV)
-		.attr("class", "area")
-		.attr("d", area);
-
-		svg.append("g")
-		.attr("class", "grid")
-		.call(yAxis)
 	}
 
 	function pieChart(id, data_value) {
@@ -344,41 +270,5 @@
 		.style("fill", function(d, i) { if(i==0) return '#ffffff'; else return '#000000'; } )
 		.style("font-size", "13px");
 	}
-</script>
-
-
-<script src="{{asset('js/swiper.min.js')}}"></script>
-<script>
-	// var swiper = new Swiper('#swiper-tab-child-output', {
-	// 	spaceBetween: 30,
-	// 	hashNavigation: {
-	// 		watchState: true,
-	// 	},
-	// 	pagination: {
-	// 		el: '.swiper-pagination',
-	// 		clickable: true,
-	// 	},
-	// 	navigation: {
-	// 		nextEl: '.swiper-button-next',
-	// 		prevEl: '.swiper-button-prev',
-	// 	},
-	// });
-</script>
-
-<script>
-	// var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-	// $('.area-date').html(months[startDate.substr(-1) - 1] + " " + startDate.substr(2,2) + ' - ' + months[endDate.substr(-1) - 1] + " " + endDate.substr(2,2));
-	// $('.area-date').html($('#trend_period_id').find(':selected').text());	
-</script>
-<script> 
-	// if(location.hash.slice(1)) {
-	// 	$('.swipernav').removeClass('active');
-	// 	$('.nav-'+ location.hash.slice(1)).addClass('active');
-	// }
-
-	// $(window).on('hashchange',function(){ 
-	// 	$('.swipernav').removeClass('active');
-	// 	$('.nav-'+ location.hash.slice(1)).addClass('active');
-	// });
 </script>
 @endsection
