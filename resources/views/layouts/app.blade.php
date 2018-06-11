98adf23ecee2fb573c85458f2b63b9a2ea0d1c63
Ruju Bajracharya <bajra.ruju@gmail.com>
2018-06-11 15:21:32 +0545

    added google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,16 @@
     <![endif]-->
 
     @yield('styles')
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-120665192-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-120665192-1');
+    </script>
+
   </head>
   <body>
     <div class="header-full">
